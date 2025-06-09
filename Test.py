7ff21ce3b6593e97b79d48318339feb7fc04332b
MutraManasaReddy <manasamutra8@gmail.com>
2025-06-09 20:52:45 +0800

    commitnig test file

--- Test.py
+++ Test.py
@@ -21,11 +21,11 @@ class CarManager:
         for car in self.cars:
             if car.car_id == car_id:
                 return car
-        return "Car not found."
+        return "Car not found"
 
     def list_cars(self):
         if not self.cars:
-            print("No cars in the system.")
+            print("No cars  avaliable")
         else:
             print("Car List:")
             for car in self.cars:
@@ -42,7 +42,7 @@ class CarManager:
                     car.year = year
                 print(f"Updated: {car}")
                 return
-        print("Car not found.")
+        print("Car not found")
 
     def remove_car(self, car_id):
         for car in self.cars:
@@ -50,44 +50,37 @@ class CarManager:
                 self.cars.remove(car)
                 print(f"Removed car with ID {car_id}")
                 return
-        print("Car not found.")
-
-    def sort_cars(self, by="year"):
-        if by == "year":
-            self.cars.sort(key=lambda x: x.year)
-        elif by == "brand":
-            self.cars.sort(key=lambda x: x.brand.lower())
-        else:
-            print("Invalid sort key. Use 'year' or 'brand'.")
-            return
-        print(f"Cars sorted by {by}.")
+        print("Car not found")
 
 
-# Example usage
+
 if __name__ == "__main__":
     manager = CarManager()
 
     # Adding cars
+
     manager.add_car(Car(1, "aaa", "green", 20223))
     manager.add_car(Car(2, "bbb", "red", 2021))
     manager.add_car(Car(3, "ccc", "Black", 2022))
 
     # Listing all cars
+
     manager.list_cars()
 
     # Getting a car by ID
+
     print("Get car with ID 2:")
     print(manager.get_car(2))
 
     # Updating a car
+
     manager.update_car(1, colour="White", year=2020)
 
     # Removing a car
+
     manager.remove_car(3)
 
-    # Sorting cars
-    manager.sort_cars(by="brand")
-    manager.list_cars()
+    
 
 
     
